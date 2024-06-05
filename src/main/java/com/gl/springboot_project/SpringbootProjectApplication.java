6a55f9dd2fe86ee363ad24ae83f68bb16b1408d8
Anish Pemmaraju <anish.pemmaraju@globallogic.com>
2024-06-05 15:23:43 +0530

    Feature 2 Added

--- src/main/java/com/gl/springboot_project/SpringbootProjectApplication.java
+++ src/main/java/com/gl/springboot_project/SpringbootProjectApplication.java
@@ -9,6 +9,7 @@ public class SpringbootProjectApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootProjectApplication.class, args);
 		System.out.println("Main Feature");
+		System.out.println("Feature 2 Added");
 	}
 
 }
